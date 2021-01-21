d55301a30fd9913d0275797ff21a33b2640d34a0
Made I.T <info@madeit.be>
2021-01-21 15:04:00 +0100

    Update wordpress.php

--- src/config/wordpress.php
+++ src/config/wordpress.php
@@ -1,5 +1,16 @@
 <?php
 
+use GuzzleHttp\Client;
+
 return [
     'app_url' => env('WORDPRESS_APP_URL', 'https://localhost'),
+    'client' => new Client([
+        'base_uri' => env('WORDPRESS_APP_URL', 'https://localhost'),
+        'timeout' => 60.0,
+        'headers' => [
+            'User-Agent' => 'Made I.T. - WordPress PHP SDK',
+            'Accept' => 'application/json',
+        ],
+        'verify' => true,
+    ]),
 ];
